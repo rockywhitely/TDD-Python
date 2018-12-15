b80b22b3d0ff80eeb2d61d5d70170fd0fc6032f7
Rocky Whitely <rockywhitely2@gmail.com>
2018-12-15 14:36:51 -0600

    Update TDD_Python

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
+from time import sleep
 
 browser = webdriver.Firefox()
 browser.get('http:\\localhost:8000')
 
 assert 'Django' in browser.title
+# browser.quit()
+
+print(browser.title)
+
+sleep(1000)
+
+browser.quit()
